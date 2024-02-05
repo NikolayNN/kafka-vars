a6824d2815482b37a534103bb7861665097b7bcf
Nikolay <nikolay.horushko@gmail.com>
2024-02-05 13:16:14 +0300

    GenericRecordFactory

--- src/main/java/by/aurorasoft/kafka/utils/GenericRecordFactory.java
+++ src/main/java/by/aurorasoft/kafka/utils/GenericRecordFactory.java
@@ -1,12 +1,12 @@
-package by.aurorasoft.kafka.consumer;
+package by.aurorasoft.kafka.utils;
 
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.reflect.ReflectData;
 
-public interface GenericRecordFactory {
-    default GenericRecord createGenericRecord(Object obj, Schema schema) {
+public class GenericRecordFactory {
+    public static GenericRecord createGenericRecord(Object obj, Schema schema) {
         GenericRecord record = new GenericData.Record(schema);
         for (Schema.Field field : schema.getFields()) {
             Object fieldValue = ReflectData.get().getField(obj, field.name(), field.pos());
