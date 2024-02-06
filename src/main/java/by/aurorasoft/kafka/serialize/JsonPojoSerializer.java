7f53fee3e815ddf9084311afe7d0b0c2b07c6b1c
Nikolay <nikolay.horushko@gmail.com>
2024-02-06 11:57:51 +0300

    add pojo serde

--- src/main/java/by/aurorasoft/kafka/serialize/JsonPojoSerializer.java
+++ src/main/java/by/aurorasoft/kafka/serialize/JsonPojoSerializer.java
@@ -4,22 +4,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Serializer;
 
-public class JsonPojoSerializer<T> implements Serializer<T> {
-        private final ObjectMapper objectMapper;
+public class JsonPojoSerializer implements Serializer<Object> {
+    private final ObjectMapper objectMapper;
 
-        public JsonPojoSerializer(ObjectMapper objectMapper) {
-            this.objectMapper = objectMapper;
-        }
+    public JsonPojoSerializer(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
-        @Override
-        public byte[] serialize(String topic, T data) {
-            if (data == null) {
-                return null;
-            }
-            try {
-                return objectMapper.writeValueAsBytes(data);
-            } catch (Exception e) {
-                throw new SerializationException("Error serializing JSON message", e);
-            }
+    @Override
+    public byte[] serialize(String topic, Object data) {
+        if (data == null) {
+            return null;
         }
+        try {
+            return objectMapper.writeValueAsBytes(data);
+        } catch (Exception e) {
+            throw new SerializationException("Error serializing JSON message", e);
+        }
+    }
 }
